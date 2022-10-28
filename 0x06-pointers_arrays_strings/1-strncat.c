d917524ac3e45f91ad7f65baa7285be849440473
sirleaftech <marksirleaf@gmail.com>
2022-10-28 14:44:29 -0700

    Updated function

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,29 +9,17 @@
  */
 
 char *_strncat(char *dest, char *src, int n)
-
 {
-
 	int i, j;
-
-
 
 	i = 0;
 
-
-
 	while (dest[i] != '\0')
-
 	{
-
 		i++;
-
 	}
 
-
-
 	j = 0;
-
 
 
 	while (src[j] != '\0' && j < n)
@@ -43,15 +31,6 @@ char *_strncat(char *dest, char *src, int n)
 		i++;
 
 		j++;
-
-
-
 	}
-
-
-
-
-
 	return (dest);
-
 }
